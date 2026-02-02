9751f2564294167a66b82676f605de0eea836311
geunnseung <bsg1017@gmail.com>
2026-02-02 19:32:19 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/geunnseung/chasewar/ChaseWarApplication.java
@@ -0,0 +1,13 @@
+package com.geunnseung.chasewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaseWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaseWarApplication.class, args);
+    }
+
+}
